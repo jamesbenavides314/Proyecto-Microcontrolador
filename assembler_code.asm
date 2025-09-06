2d4c6e763f9674e2a1291efa8315592312c13d84
jamesbenavides314 <jamesbenavides@unicauca.edu.co>
2025-09-05 19:14:04 -0500

    Actualización assembler_code.asm
    Quité código no relevante, cambié la lógica de la secuencia, ahora usa 3 bucles para el retardo, funcionan para 5s y 2s. Este código de momento debería ser capaz de encender el led durante 5 segundos y mantenerlo apagado durante 2 segundos, repitiéndose indefinidamente. Aún no se ha probado en un circuito físico, por tanto queda sujeto a cambios.

--- assembler_code.asm
+++ assembler_code.asm
@@ -1,120 +1,84 @@
-; PIC18F4550 Configuration Bit Settings
-
-; Assembly source line config statements
-
-; CONFIG1L
-  CONFIG  PLLDIV = 1            ; PLL Prescaler Selection bits (No prescale (4 MHz oscillator input drives PLL directly))
-  CONFIG  CPUDIV = OSC1_PLL2    ; System Clock Postscaler Selection bits ([Primary Oscillator Src: /1][96 MHz PLL Src: /2])
-  CONFIG  USBDIV = 1            ; USB Clock Selection bit (used in Full-Speed USB mode only; UCFG:FSEN = 1) (USB clock source comes directly from the primary oscillator block with no postscale)
-
-; CONFIG1H
-  CONFIG  FOSC = INTOSCIO_EC    ; Oscillator Selection bits (Internal oscillator, port function on RA6, EC used by USB (INTIO))
-  CONFIG  FCMEN = OFF           ; Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
-  CONFIG  IESO = OFF            ; Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
-
-; CONFIG2L
-  CONFIG  PWRT = OFF            ; Power-up Timer Enable bit (PWRT disabled)
-  CONFIG  BOR = ON              ; Brown-out Reset Enable bits (Brown-out Reset enabled in hardware only (SBOREN is disabled))
-  CONFIG  BORV = 3              ; Brown-out Reset Voltage bits (Minimum setting 2.05V)
-  CONFIG  VREGEN = OFF          ; USB Voltage Regulator Enable bit (USB voltage regulator disabled)
-
-; CONFIG2H
-  CONFIG  WDT = OFF             ; Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
-  CONFIG  WDTPS = 32768         ; Watchdog Timer Postscale Select bits (1:32768)
-
-; CONFIG3H
-  CONFIG  CCP2MX = ON           ; CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
-  CONFIG  PBADEN = OFF          ; PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
-  CONFIG  LPT1OSC = OFF         ; Low-Power Timer 1 Oscillator Enable bit (Timer1 configured for higher power operation)
-  CONFIG  MCLRE = ON            ; MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
-
-; CONFIG4L
-  CONFIG  STVREN = ON           ; Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
-  CONFIG  LVP = OFF             ; Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
-  CONFIG  ICPRT = OFF           ; Dedicated In-Circuit Debug/Programming Port (ICPORT) Enable bit (ICPORT disabled)
-  CONFIG  XINST = OFF           ; Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
-
-; CONFIG5L
-  CONFIG  CP0 = OFF             ; Code Protection bit (Block 0 (000800-001FFFh) is not code-protected)
-  CONFIG  CP1 = OFF             ; Code Protection bit (Block 1 (002000-003FFFh) is not code-protected)
-  CONFIG  CP2 = OFF             ; Code Protection bit (Block 2 (004000-005FFFh) is not code-protected)
-  CONFIG  CP3 = OFF             ; Code Protection bit (Block 3 (006000-007FFFh) is not code-protected)
-
-; CONFIG5H
-  CONFIG  CPB = OFF             ; Boot Block Code Protection bit (Boot block (000000-0007FFh) is not code-protected)
-  CONFIG  CPD = OFF             ; Data EEPROM Code Protection bit (Data EEPROM is not code-protected)
-
-; CONFIG6L
-  CONFIG  WRT0 = OFF            ; Write Protection bit (Block 0 (000800-001FFFh) is not write-protected)
-  CONFIG  WRT1 = OFF            ; Write Protection bit (Block 1 (002000-003FFFh) is not write-protected)
-  CONFIG  WRT2 = OFF            ; Write Protection bit (Block 2 (004000-005FFFh) is not write-protected)
-  CONFIG  WRT3 = OFF            ; Write Protection bit (Block 3 (006000-007FFFh) is not write-protected)
-
-; CONFIG6H
-  CONFIG  WRTC = OFF            ; Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) are not write-protected)
-  CONFIG  WRTB = OFF            ; Boot Block Write Protection bit (Boot block (000000-0007FFh) is not write-protected)
-  CONFIG  WRTD = OFF            ; Data EEPROM Write Protection bit (Data EEPROM is not write-protected)
-
-; CONFIG7L
-  CONFIG  EBTR0 = OFF           ; Table Read Protection bit (Block 0 (000800-001FFFh) is not protected from table reads executed in other blocks)
-  CONFIG  EBTR1 = OFF           ; Table Read Protection bit (Block 1 (002000-003FFFh) is not protected from table reads executed in other blocks)
-  CONFIG  EBTR2 = OFF           ; Table Read Protection bit (Block 2 (004000-005FFFh) is not protected from table reads executed in other blocks)
-  CONFIG  EBTR3 = OFF           ; Table Read Protection bit (Block 3 (006000-007FFFh) is not protected from table reads executed in other blocks)
-
-; CONFIG7H
-  CONFIG  EBTRB = OFF           ; Boot Block Table Read Protection bit (Boot block (000000-0007FFh) is not protected from table reads executed in other blocks)
-
+; Configuraci贸n de bits de configuraci贸n (Fuses)
+CONFIG  FOSC = INTOSCIO_EC   ; Usa el oscilador interno a 8 MHz
+CONFIG  WDT = OFF            ; Deshabilitar el Watchdog Timer
+CONFIG  LVP = OFF            ; Deshabilitar la programaci贸n en bajo voltaje
+CONFIG  PBADEN = OFF         ; Configurar los pines de PORTB como digitales
+CONFIG  MCLRE = OFF          ; Pin MCLR deshabilitado
+    
 #include <xc.inc>
 
-PSECT  resetVec, class=CODE, reloc=2  ; Secci髇 para el vector de reinicio
+; Secci贸n para el vector de reinicio
+PSECT  resetVec, class=CODE, reloc=2, abs
 ORG 0X00
-GOTO Inicio
-PSECT  main_code, class=CODE, reloc=2  ; Secci髇 de c骴igo principal
+    GOTO Inicio
+    
+
+; Secci贸n de c贸digo principal
+PSECT  main_code, class=CODE, reloc=2  
 
 Inicio:
-    MOVLW       0b1001000   ;Poner 8MHz en el oscilador
-    MOVWF       OSCCON
+    MOVLW   0b11110110  ;Poner 8MHz en el oscilador
+    MOVWF   OSCCON
     
     CLRF    TRISB           ; Configurar PORTB como salida (0 = salida, 1 = entrada)
     CLRF    LATB            ; Apagar todos los pines de PORTB (LED apagado inicialmente)
 
 Loop:
     BSF     LATB,0          ;Enciende el led
-    MOVLW   80
-    CALL    Retardo         ;5s encendido
+    CALL    Retardo_5       ;5s encendido
+    
     BCF     LATB,0          ;Apaga el led
-    MOVLW   32
-    CALL    Retardo         ;2s encendido
+    CALL    Retardo_2       ;2s encendido
+    
     GOTO    Loop            ; Repetir el proceso de parpadeo en bucle infinito
 
-Retardo:
-    MOVWF   Rcount
+Retardo_5:
+    MOVLW   0b11111010         ; Funci贸n para el retraso de 5000 ms
+    MOVWF   Rcount1
+    MOVLW   0b11011100
+    MOVWF   Rcount2
+    MOVLW   0b00101101
+    MOVWF   Rcount3
+    CALL    Rloop
+    RETURN
+ 
+Retardo_2:
+    MOVLW   0b11111010         ; Funci贸n para el retraso de 2000 ms
+    MOVWF   Rcount1
+    MOVLW   0b11011100
+    MOVWF   Rcount2
+    MOVLW   0b00010010
+    MOVWF   Rcount3
+    CALL    Rloop
+    RETURN  
     
 Rloop:
-    CALL    Retardo_1
-    DECFSZ  Rcount, F
-    GOTO    Rloop
-    RETURN
+    MOVF    Rcount3, W
+    MOVWF   C3    
+Loop3:
+    MOVF    Rcount2, W
+    MOVWF   C2
+Loop2:
+    MOVF    Rcount1, W
+    MOVWF   C1 
+Loop1:
+    DECFSZ  C1,F
+    GOTO    Loop1
     
-Retardo_1:
-    MOVLW   200
-    MOVWF   ContadorExterno
+    DECFSZ  C2,F
+    GOTO    Loop2
     
-LoopExterno:
-    MOVLW   250         
-    MOVWF   ContadorInterno
+    DECFSZ  C3,F
+    GOTO    Loop3
     
-LoopInterno:
-    NOP                         ; No hacer nada (consume un ciclo de instrucci髇)
-    DECFSZ  ContadorInterno, F  ; Decrementar ContadorInterno, si es cero, salta la siguiente instrucci髇
-    GOTO    LoopInterno         ; Si no es cero, repetir el bucle interno
-    DECFSZ  ContadorExterno, F  ; Decrementar ContadorExterno, si es cero, salta la siguiente instrucci髇
-    GOTO    LoopExterno         ; Si no es cero, repetir el bucle externo
     RETURN
    
-PSECT udata                     ; Secci髇 de datos sin inicializar (variables en RAM)
-ContadorExterno:   DS 1         ; Reserva 1 byte de memoria para el contador externo
-ContadorInterno:   DS 1         ; Reserva 1 byte de memoria para el contador interno
-Rcount:            DS 1
+PSECT udata                     ; Secci贸n de datos sin inicializar (variables en RAM)
+C1:                DS 1         
+C2:                DS 1         ; Reserva 1 byte de memoria para el contador interno
+C3:                DS 1
+Rcount1:           DS 1
+Rcount2:           DS 1
+Rcount3:           DS 1
     
 END
